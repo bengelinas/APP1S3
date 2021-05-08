68e1c4336e0f1140b34db1382f8b3599a42cc41a
Simon Leroux <lers0601@usherbrooke.ca>
2021-05-08 18:48:57 -0400

    preparation,retirer,addition

--- MenuFact01/code/inventaire/Inventaire.java
+++ MenuFact01/code/inventaire/Inventaire.java
@@ -2,15 +2,26 @@ package inventaire;
 
 import ingredients.Ingredient;
 import ingredients.IngredientInventaire;
+import ingredients.exceptions.IngredientException;
 
 import java.util.ArrayList;
 
 public class Inventaire {
     private ArrayList<IngredientInventaire> lesIngredients = new ArrayList<IngredientInventaire>();
 
-    public void ajouter (IngredientInventaire i)
-    {
-        lesIngredients.add(i);
+    public void ajouter (IngredientInventaire IngredientAAjouter) throws IngredientException {
+        for(int i = 0;i<lesIngredients.size();i++)
+        {
+            if (lesIngredients.get(i).getIngredient().getNom() == IngredientAAjouter.getIngredient().getNom()) {
+                try {
+                    lesIngredients.get(i).setQuantite(lesIngredients.get(i).getQuantite() + IngredientAAjouter.getQuantite());
+                } catch (IngredientException e) {
+                    throw e;
+                }
+                return;
+            }
+        }
+        lesIngredients.add(IngredientAAjouter);
     }
     public int verifier (IngredientInventaire ingredientInventaire) {
         for(int i=0; i< lesIngredients.size(); i++)
@@ -21,7 +32,8 @@ public class Inventaire {
         }
         return -1;
     }
-    public void retirer(int index, int quantite){
-
+    public void retirer(int Index, int QuantiteAEnlever)
+    {
+        lesIngredients.get(Index).retirerQuantite(QuantiteAEnlever);
     }
 }
--- MenuFact01/code/menufact/facture/Chef.java
+++ MenuFact01/code/menufact/facture/Chef.java
@@ -1,10 +1,13 @@
 package menufact.facture;
 
+import inventaire.Inventaire;
+import menufact.plats.PlatAuMenu;
+
 public class Chef implements Subscriber{
     private static Chef instance;
+
     private Chef()
     {
-
     }
     public void update(){
 
@@ -15,5 +18,13 @@ public class Chef implements Subscriber{
             instance = new Chef();
         }
         return instance;
+    }
+    private void Preparation(int[][] liste, PlatAuMenu PlatEnPreparation)
+    {
+        for (int i=0;i<liste.length;i++)
+        {
+            inventaire.retirer(liste[i][0],liste[i][1]);
         }
+        //TODO PlatEnPreparation doit etre mis en etat servi
+    }
 }
