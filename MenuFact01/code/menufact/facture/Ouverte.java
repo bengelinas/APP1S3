f9ec064b58b2cbede46cd600ab7009019f073f62
Benjamin <ben.gelinas@hotmail.com>
2021-05-10 19:06:55 -0400

    Essaye que sa explose pas

--- MenuFact01/code/menufact/facture/Ouverte.java
+++ MenuFact01/code/menufact/facture/Ouverte.java
@@ -1,9 +1,33 @@
 package menufact.facture;
 
+import menufact.facture.exceptions.FactureException;
+import menufact.plats.PlatChoisi;
+
+import java.util.ArrayList;
+
 public class Ouverte implements FactureEtat{
+
+    private Facture facture;
+    private ArrayList<PlatChoisi> platchoisi;
+    private ArrayList<Subscriber> subscriber;
+
     public Ouverte(){};
     @Override
-    public void updateFacture(){ System.out.println("La facture est en mode Ouverte"); }
+    public void payer() throws FactureException {
+        throw new FactureException("La facture doit etre Fermer avant de payer");
+    }
     @Override
-    public String getEtat() { return "Ouverte"; }
+    public void ajoutePlat(PlatChoisi p) throws FactureException {
+        p.getPlat().commander();
+        platchoisi.add(p);
+        facture.notifySubscriber(p);
+    }
+    @Override
+    public void ouvrir() throws FactureException {
+        facture.setEtat(new Ouverte());
+    }
+    @Override
+    public String genereFacture() throws FactureException {
+        throw new FactureException("Criss de beigne ta facture est pas fermee");
+    }
 }
